b7a9bda1cfce88c57f9c31db8e0b0c2ffdffa009
Les Aker <me@lesaker.org>
2015-10-23 12:43:07 -0400

    fix linting

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,5 @@
+##
+# Sets up puppet to run masterlessly via a systemd timer
 class masterless(
     $codedir = '/etc/puppetlabs/code',
     $bindir = '/usr/local/bin'
@@ -8,24 +10,24 @@ class masterless(
     $manifestpath = "${envdir}/manifests/site.pp"
 
     file { "${bindir}/puppet-run":
-        ensure => 'file',
+        ensure  => 'file',
         content => template('masterless/puppet-run.erb'),
-        mode => '0755'
+        mode    => '0755'
     }
 
     file { '/etc/systemd/system/puppet-run.service':
-        ensure => 'file',
+        ensure  => 'file',
         content => template('masterless/puppet-run.service.erb')
     }
 
     file { '/etc/systemd/system/puppet-run.timer':
-        ensure => 'file',
+        ensure  => 'file',
         content => template('masterless/puppet-run.timer.erb')
     }
 
     file { '/etc/systemd/system/multi-user.target.wants/puppet-run.timer':
-        ensure => 'link',
-        target => '/etc/systemd/system/puppet-run.timer',
+        ensure  => 'link',
+        target  => '/etc/systemd/system/puppet-run.timer',
         require => File['/etc/systemd/system/puppet-run.timer']
     }
 }
