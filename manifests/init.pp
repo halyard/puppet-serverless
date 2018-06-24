69fdfe6ef7b511459f0b6f3ea130143d8673e883
Les Aker <me@lesaker.org>
2018-06-24 17:10:08 -0400

    tidy logs, fixes #3

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,7 @@
 # Sets up puppet to run masterlessly via a systemd timer
 class masterless(
     $repodir = '/opt/halyard/repo',
+    $logdir = '/opt/halyard/logs',
     $bindir = '/usr/local/bin',
     $bootdelay = '1min',
     $frequency = '3600'
@@ -10,5 +11,11 @@ class masterless(
         'Darwin': { include masterless::darwin }
         'Archlinux': { include masterless::systemd }
         default: { fail("Module does not support ${::osfamily}") }
+    }
+
+    tidy { $logdir:
+      age     => '90d',
+      recurse => true,
+      matches => 'puppet-run.*',
     }
 }
